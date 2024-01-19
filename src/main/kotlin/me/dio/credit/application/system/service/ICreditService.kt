d01ba8576ea80ce9b306a719975bbddd3c8c338d
chrissperb <chrissperb@gmail.com>
2024-01-19 20:30:06 -0300

    Service layer and interfaces created.

---
+++ src/main/kotlin/me/dio/credit/application/system/service/ICreditService.kt
@@ -0,0 +1,11 @@
+package me.dio.credit.application.system.service
+
+import me.dio.credit.application.system.entity.Credit
+import org.aspectj.apache.bcel.classfile.Code
+import java.util.UUID
+
+interface ICreditService {
+    fun save(credit: Credit): Credit
+    fun findAllByCustomer(customerId: Long): List<Credit>
+    fun finndByCreditCode(creditCode: UUID):Credit
+}
