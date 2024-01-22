ce78da2db4d07b00ca167a59feecc9cff8d5e43b
chrissperb <chrissperb@gmail.com>
2024-01-22 00:29:32 -0300

    Controller layer package builded, customer services initiated.

---
+++ src/main/kotlin/me/dio/credit/application/system/dto/CustomerDto.kt
@@ -0,0 +1,30 @@
+package me.dio.credit.application.system.dto
+
+import me.dio.credit.application.system.entity.Address
+import me.dio.credit.application.system.entity.Customer
+import java.math.BigDecimal
+
+data class CustomerDto(
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val income: BigDecimal,
+    val email: String,
+    val password: String,
+    var zipCode: String,
+    val street: String
+) {
+
+    fun toEntity(): Customer = Customer(
+            firstName = this.firstName,
+            lastName = this.lastName,
+            cpf = this.cpf,
+            email = this.email,
+            income = this.income,
+            password = this.password,
+            address = Address (
+                zipCode = this.zipCode,
+                street = this.street
+            )
+        )
+    }
